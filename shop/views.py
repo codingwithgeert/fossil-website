8414caabe6d320b2acf25c8b472b13c5edae036c
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-11-22 22:09:13 +0000

    Changed product_item to product_objects

--- shop/views.py
+++ shop/views.py
@@ -4,6 +4,6 @@ from .models import Product
 # Create your views here.
 
 def index(request):
-    product_item = Product.object.all()
-    return render(request, 'shop/index.html', {'product_item':product_item})
+    product_objects = Product.objects.all()
+    return render(request, 'shop/index.html', {'product_objects':product_objects})
     
