5d1d985d4156e35afd2a4f367b6eb8bd58bac136
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-11-22 21:55:06 +0000

    index request added

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    product_item = Product.object.all()
+    return render(request, 'shop/index.html', {'product_item':product_item})
+    
