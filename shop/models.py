a2c7b553710a2507d6a6049466786fc11d750691
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-11-27 19:47:25 +0000

    Changed product to products

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
+class Products(models.Model):
     title = models.CharField(max_length=250)
     price = models.FloatField()
     discount_price = models.FloatField()
