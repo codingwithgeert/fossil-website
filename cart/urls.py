e99e81d7ecc738af07e1f2524bed6ccd09b6f64c
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-07 20:23:17 +0000

    urls added for add to cart and adjust cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
-from .views import view_cart, add_to_cart, adjust_cart, path, url
+from django.urls import path, include
+from .views import view_cart, add_to_cart, adjust_cart
 
 urlpatterns = [
     path('', view_cart, name="view_cart"),
-     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
-     url(r'^adjust/(?P<id>\d+)', adjust_cart, name='adjust_cart'),
+    path('add/<int:pk>/', add_to_cart, name='add_to_cart'),
+    path('adjust/<int:pk>/', adjust_cart, name='adjust_cart'),
     ]
+    
+    
