4790a02c5838bc11a7ac3aa88dbf1ab5cf461bae
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-13 20:43:06 +0000

    These files and app not needed as function work in shop

--- pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from shop.models import Products
-from pages.views import detail_page
-
-# Create your views here.
-def detail_page(request,id):
-    product_object = Products.objects.get(id=id)
-    return render(request,'pages/details.html',{'product_object':product_object})
