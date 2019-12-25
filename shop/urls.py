cc5c25f538a702b17688b956f10008ac7516126f
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-25 17:12:34 +0000

    changed the .views

---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from shop import views as shop_views
+from cart import views as cart_views
+from django.conf.urls.static import static, serve
+from .settings import MEDIA_ROOT
+from django.conf import settings
+
+
+urlpatterns = [
+    path('',shop_views.index,name='index' ),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT }),
+    path('<int:id>/',shop_views.detailPage,name='detail',),
+    
+]
