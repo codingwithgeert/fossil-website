0044e864e01c92b96c22a445a4919111a435d2dc
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-25 19:24:43 +0000

    added view to render cart.html

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def shop_view(request):
-    pass
-
-def cartPage(request):
-    return render(request, 'cart.html')
+def view_cart(request):
+    """A View that renders the cart contents page"""
+    return render(request, "cart/cart.html")
