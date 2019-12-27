3014e4e6d6349bdf6bffc88ee9da0f849cb01b59
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-27 12:09:54 +0000

    Added _cart_id to create a session

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from shop.models import Products, Cart, CartItem
 
 # Create your views here.
 def view_cart(request):
     """A View that renders the cart contents page"""
     return render(request, "cart/cart.html")
+    
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
