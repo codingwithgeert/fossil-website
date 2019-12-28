af0b843277df1e7b64f7d6c90a408b373171df68
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-28 14:34:55 +0000

    fixed typo

--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,8 @@ def _cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
-    
+
+
 def add_cart(request, product_id):
     product = Products.objects.get(id=product_id)
     try:
@@ -24,7 +25,7 @@ def add_cart(request, product_id):
         cart.save()
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
-        if cart_item.quantity < cart_item.quantity:
+        if cart_item.quantity:
             cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -36,7 +37,8 @@ def add_cart(request, product_id):
         cart_item.save()
 
     return redirect('cart_detail')
-    
+
+
 def cart_detail(request, total=0, counter=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -46,7 +48,5 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
             counter += cart_item.quantity
     except ObjectDoesNotExist:
         pass
-    return render(request, 'cart/cart.html', dict(cart_items = cart_items, total = total, counter= counter))
-
-
-        
+    
+    return render(request, 'cart/cart.html', dict(cart_items=cart_items, total=total, counter=counter,))
