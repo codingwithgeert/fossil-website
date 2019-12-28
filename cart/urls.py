5158ea5ac866abc2fcb9f6a1cfb60528fa5baa7a
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-28 15:07:03 +0000

    remove url added to cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,6 @@ from cart import views as cart_views
 urlpatterns = [
     path('',view_cart,name='view_cart'),
     path('cart/add/<int:product_id>', cart_views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', cart_views.cart_remove, name='cart_remove'),
     path('cart', cart_views.cart_detail, name='cart_detail'),
     ]
