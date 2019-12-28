560bed227597983bf58a8cc5b681bf7188ae8d1f
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2019-12-28 13:14:36 +0000

    deleted the links not needed

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from shop import views as shop_views
+from cart import views as cart_views
 from django.conf.urls.static import static, serve
 from .settings import MEDIA_ROOT
 from django.conf import settings
@@ -11,5 +12,6 @@ urlpatterns = [
     path('',shop_views.index,name='index' ),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT }),
     path('<int:id>/',shop_views.detailPage,name='detail',),
+    path('cart/add/<int:product_id>', cart_views.add_cart, name='add_cart'),
   
 ]
