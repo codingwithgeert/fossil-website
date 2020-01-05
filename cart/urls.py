1bb72139b816bfc73b1108db736602a5bc19f81f
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-05 10:53:50 +0000

    urls added for cart add, cart remove and cart trashbin

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',view_cart,name='view_cart'),
     path('cart/add/<int:product_id>', cart_views.add_cart, name='add_cart'),
     path('cart/remove/<int:product_id>', cart_views.cart_remove, name='cart_remove'),
+    path('cart/trashbin_product/<int:product_id>', cart_views.trashbin_product, name='trashbin_product'),
     path('cart', cart_views.cart_detail, name='cart_detail'),
     ]
