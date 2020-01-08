f65d14e3c1bea03f6618373d50fcfbb2a9224205
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-08 21:34:08 +0000

    urlpattern created for path thankyou/<int:order_id>

--- cart/urls.py
+++ cart/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('cart/remove/<int:product_id>', cart_views.cart_remove, name='cart_remove'),
     path('cart/trashbin_product/<int:product_id>', cart_views.trashbin_product, name='trashbin_product'),
     path('cart', cart_views.cart_detail, name='cart_detail'),
+    path('thankyou/<int:order_id>', cart_views.thank_you, name='thank_you'),
     ]
