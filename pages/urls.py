b5ceb0c03d7870c97bd45dc8370fd7cb31e373ce
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-19 17:35:52 +0000

    url faq added

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from pages import views as pages_views
 
 urlpatterns = [
     path('pages/about/', pages_views.about_me, name='about'),
+    path('pages/faq/', pages_views.faq_page, name='faq'),
     ]
