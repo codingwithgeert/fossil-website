9e77cf9fef656e3a9ddbb8de7009042f1dedf09a
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-19 17:36:58 +0000

    added faq_page view

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,6 @@ def about_me(request):
     A View that renders the about me page
     """
     return render(request, "about.html")
+    
+def faq_page(request):
+    return render(request, "faq.html")
