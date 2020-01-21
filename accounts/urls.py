73feea9a7e5b35ef0aa19d5d2b75bfc530b9d0a1
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-21 22:44:24 +0000

    added all

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from accounts import views as accounts_views
 
 urlpatterns = [
-    path('account/acount/create/', accounts_views.signupView, name='signup'),
+    path('account/create/', accounts_views.signupView, name='signup'),
     path('account/login/', accounts_views.loginView, name='login'),
-    path('account/logout', accounts_views.logoutView, name='logout'),
+    path('account/logout/', accounts_views.logoutView, name='logout'),
     path('order_history/', accounts_views.orderHistory, name='order_history'),
     ]
