fa1a61d848097cbc852b8897c347e178bb658143
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-24 16:44:16 +0000

    ContacytUsForm class added

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,4 +11,10 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2' )
     
+class ContactUsForm(forms.Form):
+    subject = forms.CharField(max_length=100, required=True)
+    name = forms.CharField(max_length=50, required=True)
+    from_email = forms.EmailField(max_length=50, required=True)
+    message = forms.CharField(max_length=100, required=True)
+
     
