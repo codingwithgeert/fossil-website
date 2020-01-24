6d70b5476ff3a54fadfa2ef8cf038baab96f9639
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-24 14:41:01 +0000

    deleted the word pages in url as it was not needed

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from pages import views as pages_views
 
 urlpatterns = [
-    path('pages/about/', pages_views.about_me, name='about'),
-    path('pages/faq/', pages_views.faq_page, name='faq'),
+    path('about/', pages_views.about_me, name='about'),
+    path('faq/', pages_views.faq_page, name='faq'),
     ]
