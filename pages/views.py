1dcf4a9b2ee4fe0aaaf7e3ef26259c2cd57cc40d
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-01-25 14:55:34 +0000

    changed email to contact@geertdeveloper.online

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from accounts.forms import ContactUsForm 
+from django.core.mail import EmailMessage
 # Create your views here.
 def about_me(request):
     """
@@ -11,5 +12,26 @@ def faq_page(request):
     return render(request, "faq.html")
 
 def contact(request):
-    form = ContactUsForm()
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data.get('subject')
+            from_email = form.cleaned_data.get('from_email')
+            message = form.cleaned_data.get('message')
+            name = form.cleaned_data.get('name')
+            
+            message_format = "{0} has sent you a message\n\n{1}".format(name, message)
+            msg = EmailMessage(
+            subject, 
+            message_format, 
+            to=['contact@geertdeveloper.online'], 
+            from_email=from_email
+            )
+            msg.send()
+            
+            return render(request, "contact_send.html")
+    
+    else:
+        form = ContactUsForm
+        
     return render(request, "contact.html", {'form': form})
