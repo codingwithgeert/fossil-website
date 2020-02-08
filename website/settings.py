0296d05cb08f46c435282fd7ea45ab3650da28bb
Ubuntu <ubuntu@ip-172-31-46-202.ec2.internal>
2020-02-08 21:34:52 +0000

    deleted the comments at database if else so it works again

--- website/settings.py
+++ website/settings.py
@@ -101,6 +101,12 @@ else:
        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
